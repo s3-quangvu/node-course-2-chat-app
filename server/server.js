6798cafa7a9d57e009b77779256280c5b9382db1
vu.tranq@s3corp.com.vn <vu.tranq@s3corp.com.vn>
2018-12-24 10:45:30 +0700

    Add connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const  express = require('express');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '../public');
@@ -8,8 +10,17 @@ console.log(publicPath);
 const port = process.env.PORT || 3000
 
 var app = express();
+var server = http.createServer(app)
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+       console.log('User was disconnect');
+    });
+})
+server.listen(port, () => {
     console.log(`Server is up listen port ${port}`);
 })
